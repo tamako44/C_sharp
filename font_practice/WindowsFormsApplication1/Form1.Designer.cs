7a511b257267d1c16879c1ea0e2c748813260f6d
Yushan Chan <tamako.jhan@gmail.com>
2017-07-03 18:09:39 +0900

    Button implement

---
+++ font_practice/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_label = new System.Windows.Forms.Label();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(35, 31);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(75, 23);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "Show text";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_label
+            // 
+            this.btn_label.AutoSize = true;
+            this.btn_label.Font = new System.Drawing.Font("Berlin Sans FB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_label.Location = new System.Drawing.Point(225, 31);
+            this.btn_label.Name = "btn_label";
+            this.btn_label.Size = new System.Drawing.Size(0, 40);
+            this.btn_label.TabIndex = 1;
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(35, 60);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(109, 23);
+            this.btn_2.TabIndex = 2;
+            this.btn_2.Text = "Replace and Resize";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(35, 89);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(106, 23);
+            this.btn_3.TabIndex = 3;
+            this.btn_3.Text = "Color and Location";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 384);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_label);
+            this.Controls.Add(this.btn_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Label btn_label;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+    }
+}
+
