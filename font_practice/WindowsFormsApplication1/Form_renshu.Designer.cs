1eee506a5892b5092caf67ab2718a3f5a2e1edb8
Yushan Chan <tamako.jhan@gmail.com>
2017-07-04 16:07:46 +0900

    1)Rename and delete form 2)Implement function

---
+++ font_practice/WindowsFormsApplication1/Form_renshu.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form_renshu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_label = new System.Windows.Forms.Label();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.check_label = new System.Windows.Forms.Label();
+            this.radio_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_label
+            // 
+            this.btn_label.AutoSize = true;
+            this.btn_label.Font = new System.Drawing.Font("Berlin Sans FB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_label.Location = new System.Drawing.Point(225, 31);
+            this.btn_label.Name = "btn_label";
+            this.btn_label.Size = new System.Drawing.Size(0, 40);
+            this.btn_label.TabIndex = 1;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(35, 31);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(75, 23);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "Show text";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(35, 60);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(122, 23);
+            this.btn_2.TabIndex = 2;
+            this.btn_2.Text = "Replace and Resize";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(35, 89);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(106, 23);
+            this.btn_3.TabIndex = 3;
+            this.btn_3.Text = "Color and Location";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.Location = new System.Drawing.Point(35, 119);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(75, 23);
+            this.btn_4.TabIndex = 12;
+            this.btn_4.Text = "Reset";
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(35, 196);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Change text";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(35, 219);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(84, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Change size";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(35, 242);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(89, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Change color";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(35, 265);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(103, 17);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "Change location";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(35, 333);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(82, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Change text";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(35, 357);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Change size";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(35, 381);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(88, 17);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Change color";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(35, 405);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(102, 17);
+            this.radioButton4.TabIndex = 11;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Change location";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // check_label
+            // 
+            this.check_label.AutoSize = true;
+            this.check_label.Location = new System.Drawing.Point(343, 242);
+            this.check_label.Name = "check_label";
+            this.check_label.Size = new System.Drawing.Size(58, 13);
+            this.check_label.TabIndex = 13;
+            this.check_label.Text = "Yokohama";
+            // 
+            // radio_label
+            // 
+            this.radio_label.AutoSize = true;
+            this.radio_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radio_label.Location = new System.Drawing.Point(333, 369);
+            this.radio_label.Name = "radio_label";
+            this.radio_label.Size = new System.Drawing.Size(209, 42);
+            this.radio_label.TabIndex = 14;
+            this.radio_label.Text = "Minatomirai";
+            // 
+            // Button
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 494);
+            this.Controls.Add(this.radio_label);
+            this.Controls.Add(this.check_label);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_label);
+            this.Controls.Add(this.btn_1);
+            this.Name = "Button";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Label btn_label;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Label check_label;
+        private System.Windows.Forms.Label radio_label;
+    }
+}
+
---
+++ font_practice/WindowsFormsApplication1/Form_renshu.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form_renshu : Form
+    {
+
+        
+        public Form_renshu()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            //MSDN have .this at head
+            btn_label.Text = "Fahne";
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            //btn_label.Font = new Font("Arial",40);
+            btn_label.Font = new Font("Arial", rnd_num());
+            btn_label.Text = "Fahne Inc";
+            //btn_label.Font = new System.Drawing.Font(l.Font.FontFamily.Name, 12);
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            btn_label.Location = new Point(rnd_num(), rnd_num());
+            btn_label.ForeColor = Color.Red;
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            btn_label.Font = null;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //check_label.ResetText();
+            check_label.Text = "Fahne Inc";
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            check_label.Font = new Font("Arial", rnd_num());
+        }
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            check_label.ForeColor = Color.Yellow;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            check_label.Location = new Point(rnd_num(), rnd_num());
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                radio_label.Text = "Kanagawa ken";
+            else if (radioButton2.Checked)
+                radio_label.Font = new Font("Arial", rnd_num());
+            else if (radioButton3.Checked)
+                radio_label.ForeColor = Color.Blue;
+            else
+                radio_label.Location = new Point(rnd_num(), rnd_num());
+        }
+
+        
+        private static int rnd_num()
+        {
+            Random rnd_size = new Random();
+            int size = rnd_size.Next(10,400);
+            return size;  
+        }
+
+       
+
+    }
+}
