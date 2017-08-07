4e75e458946e35579d6e73bcb424f62e44854d4b
tamako44 <tamako.jhan@gmail.com>
2017-08-08 02:20:51 +0900

    Implemented stack and push

--- Caculator/Caculator/Form1.cs
+++ Caculator/Caculator/Form1.cs
@@ -18,15 +18,18 @@ namespace Calculator
         Status inputStatus = Status.init;
         //int inputStatus = (int)Status.init;
 
+        Stack<string> inputStack = new Stack<string>();
+
         string[] numberArray = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+
         string buttonText;
 
         string num1String = "0";
         string num2String = "0";
-        string operatorSign;
         string operatorSignTmp;
-        int num1;
-        int num2;
+        int num1 = -1;
+        int num2 = -1;
+        string operatorSign = null;
         int result;
         int resultTmp;
 
@@ -37,151 +40,73 @@ namespace Calculator
             CreateLabel();
         }
 
-        private void displayText(object sender, EventArgs e)
+        public void calculate(object sender, EventArgs e)
         {
             Button buttonText = (Button)sender;
-            this.DisplayLabel.Text = buttonText.Text;
-            Console.WriteLine(DisplayLabel.Text);
-        }
+            string input = buttonText.Text;
 
-        public void calculateInput(object sender, EventArgs e)
-        {
-            Button buttonText = (Button)sender;
-            string buttonString = buttonText.Text;
+            //bool isNumber = numberArray.Contains(input);
 
-            switch (buttonString)
+            if (input != "=")
             {
-                case "C":
-                    num1String = "0";
-                    num2String = "0";
-                    result = 0;
-                    DisplayLabel.Text = "0";
-                    inputStatus = Status.init;
-                    break;
-                default:
-                    inputStatusChange(buttonString);
-
-                    if (inputStatus == Status.num1)
-                    {
-                        num1 = Int32.Parse(num1String);
-                        DisplayLabel.Text = num1.ToString();
-                    }
-                    else if (inputStatus == Status.num2)
-                    {
-                        num2 = Int32.Parse(num2String);
-                        DisplayLabel.Text = num2.ToString();
-                    }
-                    else
-                    {
-                        switch (operatorSign)
-                        {
-                            case "+":
-                                resultTmp = result;
-                                resultTmp = num1 + num2;
-                                result += resultTmp;
-                                //addFunction();
-                                break;
-                            case "-":
-                                //resultTmp = result;
-                                //resultTmp = num1 - num2;
-                                //result -= resultTmp;
-                                //subtractFunction();
-                                break;
-                            case "=":
-                                resultTmp = result;
-                                resultTmp = num1 + num2;
-                                result += resultTmp;
-                                DisplayLabel.Text = num1.ToString();
-                                num1String = "0";
-                                num2String = "0";
-                                inputStatus = Status.num1;
-                                break;
-                            default:
-
-                                break;
-                        }                
-                    }
-                    break;
-            }          
-        }
-
-        private void inputStatusChange(string buttonString)
-        {
-            bool isNumber = numberArray.Contains(buttonString);
-
-            switch (inputStatus)
-            {
-                case Status.init:
-                    if (isNumber)
-                    {
-                        inputStatus = Status.num1;
-                        num1String += buttonString;
-                    }
-                    else
-                    {
-                        inputStatus = Status.init;
-                        operatorSign = buttonString;
-                    }
-                    break;
-                case Status.num1:
-                    if (isNumber)
-                    {
-                        inputStatus = Status.num1;
-                        num1String += buttonString;
-                    }
-                    else
-                    {
-                        inputStatus = Status.operatorSign;
-                        operatorSign = buttonString;
-                    }
-                    break;
-                case Status.operatorSign:
-                    if (isNumber)
-                    {
-                        inputStatus = Status.num2;
-                        num2String += buttonString;
-                    }
-                    else
-                    {
-                        inputStatus = Status.init;
-                        operatorSign = buttonString;
-                    }
-                    break;
-                case Status.num2:
-                    if (isNumber)
-                    {
-                        inputStatus = Status.num2;
-                        num2String += buttonString;
-                    }
-                    else
-                    {
-                        inputStatus = Status.init;
-                        operatorSign = buttonString;
-                    }
-                    break;
-                case Status.result:
-
-                    break;
-            }
-            num1 = Int32.Parse(num1String);
-            num2 = Int32.Parse(num2String);
-        }
-
-       
-
-        public void checkNumber(string buttonString)
-        {
-            bool isNumber = numberArray.Contains(buttonText);
-
-            if (isNumber)
-            {
-                buttonText += buttonString;
+                string popTmp = inputStack.Pop();
+                if (isNumber(inputStack.Pop()) && isNumber(input) )
+                {
+                    popTmp += input;
+                    inputStack.Push(popTmp);
+                }
+                else
+                {
+                    inputStack.Push(input);
+                } 
             }
             else
             {
-                buttonText = buttonString;
-            }
+                foreach (string pop in inputStack)
+                {
+                    if (isNumber(pop.Substring(0, 1)))
+                    {
+                        if (num1 == -1)
+                        {
+                            num1 = Int32.Parse(pop);
+                        }
+                        else
+                        {
+                            num2 = Int32.Parse(pop);
+                        }
+                    }
+                    else
+                    {
+                        operatorSign = pop;
+                    }
 
+                    if ( (num1 != -1) && (num2 != -1) )
+                    {
+                        switch(operatorSign)
+                        {
+                            case "+":
+                                num1 = num2 + num1;
+                                break;
+                            case "-":
+                                num1 = num2 - num1;
+                                break;
+                            case "x":
+                                num1 = num2 * num1;
+                                break;
+                            case "/":
+                                num1 = num2 / num1;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        public bool isNumber(string input)
+        {
+            return numberArray.Contains(input);
         }
 
     }
